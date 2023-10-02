d9c02a3dbb7d6f1295152a2fab35ded6c70f7691
Noah <noah7520@outlook.com>
2023-10-02 14:34:53 +0200

    Andra test

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 System.Console.WriteLine("hejdär1111");
+System.Console.WriteLine("hejejheheheheh");
+
